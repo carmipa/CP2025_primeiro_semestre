8fb28e66f4b95738953cf395d8972c36d4e11187
carmipa <pabcarminati@gmail.com>
2025-03-12 13:08:20 -0300

    primeiro commit reorganizando cps das materias

---
+++ Java_Advanced/projeto-cp-bank/bank-cp/src/main/java/br/com/fiap/bank/main/BankCpApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bank.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCpApplication.class, args);
+	}
+
+}
