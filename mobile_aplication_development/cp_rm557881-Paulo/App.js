3035445e1b80d880c0eed00869044d271201c169
carmipa <pabcarminati@gmail.com>
2025-03-14 20:21:10 -0300

    correção e rodando

--- mobile_aplication_development/cp_rm557881-Paulo/App.js
+++ mobile_aplication_development/cp_rm557881-Paulo/App.js
@@ -1,49 +1,62 @@
 import { useState } from 'react';
 import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
+function CalculadoraPreco({ valorProduto, porcentagemAumento }) {
+  const valor = parseFloat(valorProduto);
+  const aumento = parseFloat(porcentagemAumento);
+  const novoValor = valor + (valor * aumento) / 100;
+
+  return (
+    <View>
+      <Text>O novo valor do produto será: R$ {novoValor.toFixed(2)}</Text>
+    </View>
+  );
+}
+
 export default function App() {
+  const [nomeProduto, setNomeProduto] = useState('');
   const [valorProduto, setValorProduto] = useState('');
   const [porcentagemAumento, setPorcentagemAumento] = useState('');
-  const [monstrarDados, setMostrarDados] = useState(false);
-
+  const [mostrarDados, setMostrarDados] = useState(false);
 
   return (
     <View style={styles.container}>
       <Image
-
-        source={{ uri: 'https://i.scdn.co/image/ab6765630000ba8a9543f1ed639f9830d951f154' }}
+        source={{
+          uri: 'https://static.vecteezy.com/ti/vetor-gratis/p1/6624464-conceito-compras-com-bolsas-fundo-ilustracao-eps-10-gratis-vetor.jpg',
+        }}
         style={styles.imagem}
       />
+
       <TextInput
         style={styles.input}
-        placeholder='Digite o valor do produto:'
+        placeholder="Digite o nome do produto:"
+        value={nomeProduto}
+        onChangeText={(nome) => setNomeProduto(nome)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o valor do produto:"
         maxLength={10}
-        keyboardType='numeric'
+        keyboardType="numeric"
         value={valorProduto}
         onChangeText={(valor) => setValorProduto(valor)}
-
       />
-      <Text style={{ color: 'red' }}>{valorProduto}</Text>
-
       <TextInput
         style={styles.input}
-        placeholder='Porcentagem de aumento:'
+        placeholder="Porcentagem de aumento:"
         maxLength={10}
-        keyboardType='numeric'
+        keyboardType="numeric"
         value={porcentagemAumento}
         onChangeText={(valor) => setPorcentagemAumento(valor)}
-
       />
-      <Text style={{ color: 'red' }}>{porcentagemAumento}</Text>
-
-      <Button title='Calcular'
-
-        onPress={() => setMostrarDados(true)}
-      />
-      {monstrarDados && <Text>{valorProduto} - {porcentagemAumento}</Text>}
-
-
-
+      <Button title="Calcular" onPress={() => setMostrarDados(true)} />
+      {mostrarDados && (
+        <CalculadoraPreco
+          valorProduto={valorProduto}
+          porcentagemAumento={porcentagemAumento}
+        />
+      )}
     </View>
   );
 }
@@ -51,23 +64,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    gap: 10
-  },
-  imagem: {
-    resizeMode: 'center',
-    width: 300,
-    height: 300
+    padding: 16,
+    backgroundColor: '#fbd69e'
   },
   input: {
-    backgroundColor: 'white',
-    width: 400,
-    borderRadius: 5,
-    paddingLeft: 10,
-    borderWidth: 2,
-    borderColor: '#87DEEB',
-    fontSize: 20
-  }
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    width: '80%',
+    paddingHorizontal: 8,
+    backgroundColor: "white"
+  },
+  imagem: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
 });
